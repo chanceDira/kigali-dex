f460706a3e3a621ae4c0864a9212b23d3558579c
chanceDira <chancedesire60@gmail.com>
2022-01-04 20:11:36 +0200

    Updates

---
+++ precache-manifest.67b24e77de7d0d7f8df2aacd1ca739de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "93c6cf3fb95a73b544d0",
+    "url": "/kigali-dex/static/css/main.02277aab.chunk.css"
+  },
+  {
+    "revision": "93c6cf3fb95a73b544d0",
+    "url": "/kigali-dex/static/js/main.93c6cf3f.chunk.js"
+  },
+  {
+    "revision": "ccda2ed0a71701df8331",
+    "url": "/kigali-dex/static/js/runtime~main.ccda2ed0.js"
+  },
+  {
+    "revision": "7c45c16e5000674cee07",
+    "url": "/kigali-dex/static/css/2.8e6c906f.chunk.css"
+  },
+  {
+    "revision": "7c45c16e5000674cee07",
+    "url": "/kigali-dex/static/js/2.7c45c16e.chunk.js"
+  },
+  {
+    "revision": "16542870bcc4c1aa90c18644a37ccc89",
+    "url": "/kigali-dex/index.html"
+  }
+];
